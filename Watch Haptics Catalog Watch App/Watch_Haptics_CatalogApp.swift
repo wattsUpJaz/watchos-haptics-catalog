76aea44a87cc6ff0d6a3271187d301ebb973995f
Jazzy <15367315+wattsUpJaz@users.noreply.github.com>
2023-05-09 11:35:14 -0600

    Initial Commit

---
+++ Watch Haptics Catalog Watch App/Watch_Haptics_CatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_Haptics_CatalogApp.swift
+//  Watch Haptics Catalog Watch App
+//
+//  Created by Jasmyne Carson on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Watch_Haptics_Catalog_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
