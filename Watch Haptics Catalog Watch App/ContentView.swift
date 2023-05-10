5a97ed8d013b6f98d36aed818663900fb0bc1800
Jazzy <15367315+wattsUpJaz@users.noreply.github.com>
2023-05-09 19:46:17 -0600

    Add scrollView of buttons that trigger the watch's built-in haptic effects.

--- Watch Haptics Catalog Watch App/ContentView.swift
+++ Watch Haptics Catalog Watch App/ContentView.swift
@@ -9,11 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView {
+            VStack {
+                Button("notification", action: {playHaptic(hapticType: WKHapticType.notification)})
+                Button("direcitonUp", action: {playHaptic(hapticType: WKHapticType.directionUp)})
+                Button("directionDown", action: {playHaptic(hapticType: WKHapticType.directionDown)})
+                Button("success", action: {playHaptic(hapticType: WKHapticType.success)})
+                Button("failure", action: {playHaptic(hapticType: WKHapticType.failure)})
+                Button("retry", action: {playHaptic(hapticType: WKHapticType.retry)})
+                Button("start", action: {playHaptic(hapticType: WKHapticType.start)})
+                Button("stop", action: {playHaptic(hapticType: WKHapticType.stop)})
+                Button("click", action: {playHaptic(hapticType: WKHapticType.click)})
+            }
+            
         }
         .padding()
     }
@@ -23,4 +31,8 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+func playHaptic(hapticType: WKHapticType) {
+    WKInterfaceDevice.current().play(hapticType)
 }
